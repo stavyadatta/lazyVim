c52cb6ead384af7ca9c5c357dca3d36c4c4eeecd
Stavya Datta <dattastavya@gmail.com>
2025-12-26 22:38:44 +0530

    Added color theme

---
+++ lua/plugins/tokynight.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      style = "storm", -- storm | night | moon | day
+      transparent = false, -- keep false
+      dim_inactive = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
